90a9d29e41d30e234c8ac28c3883229804972c5e
Tata Pestova <tata.pestova@inbox.ru>
2023-05-31 21:39:05 +0300

    add main and about page

--- hexlet_django_blog/urls.py
+++ hexlet_django_blog/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from hexlet_django_blog import views
+
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
+    path('about/', views.about),
 ]
